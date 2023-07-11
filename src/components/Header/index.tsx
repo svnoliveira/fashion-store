61ac0dfdb7672cf3252cdf91ab305435bc9d11ce
svnoliveira <srkuran@gmail.com>
2023-07-11 19:39:37 -0300

    feat: Added images to Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,17 @@
+import logo from "@/assets/fashion-store.svg"
+import Image from "next/image"
+import { RiShoppingCart2Line } from "react-icons/ri"
+
 export const Header = () => {
-    return(
-        <header>
-            <img src="" alt="FASHIONSTORE LOGO" />
-            <img src="" alt="CART ICON" />
+    return (
+        <header className="bg-white">
+            <Image
+                src={logo}
+                width={500}
+                height={500}
+                alt="Fashion store logo"
+            />
+            <RiShoppingCart2Line fill="000000" size={40}/>
         </header>
     )
 }
