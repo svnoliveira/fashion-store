6cc0d3ac63ed6d0c499136a2d5eb9f510e1803c1
svnoliveira <srkuran@gmail.com>
2023-07-11 11:35:28 -0300

    feat: created User zustand store feat: created logout, login, load and register fuctions

---
+++ src/stores/@userTypes.ts
@@ -0,0 +1,31 @@
+export interface IUser{
+    email: string;
+    name: string;
+    id: number;
+};
+
+export interface IUserData{
+    accessToken: string;
+    user: IUser;
+};
+
+export interface ILoginData{
+    email: string;
+    password: string;
+}
+
+export interface IRegisterData{
+    email: string;
+    password: string;
+    name: string;
+}
+
+export interface IUserState {
+    loading: boolean;
+    error: string;
+    userData: null | IUserData;
+    logoutUser: () => void;
+    loginUser: ({ email, password }:ILoginData) => Promise<void>;
+    loadUser: () => void;
+    registerUser: ({ email, password, name }: IRegisterData) => Promise<true | undefined>
+}
---
+++ src/stores/useUserStore.ts
@@ -0,0 +1,56 @@
+"use client"
+import { create } from 'zustand';
+import { IUserData, IUserState } from './@userTypes';
+import { api } from '@/services/api';
+
+export const useUserStore = create<IUserState>()((set) => ({
+  loading: false,
+  error: "",
+  userData: null,
+  
+  logoutUser: () => {
+    localStorage.removeItem("@FS: userData");
+    set({ userData: null });
+  },
+
+  loginUser: async ({ email, password }) => {
+    try {
+      set({ loading: true });
+      const { data } = await api.post<IUserData>("/login", {
+        email: email,
+        password: password,
+      });
+      localStorage.setItem("@FS: userData", JSON.stringify(data));
+      set({ userData: data, error: "" });
+    } catch (error) {
+      console.log(error);
+      set({ error: "Login atempt Failed" });
+    } finally {
+      set({ loading: false });
+    };
+  },
+
+  loadUser: () => {
+    const savedUser = localStorage.getItem("@FS: userData");
+    if (savedUser) {
+      set({ userData: JSON.parse(savedUser) });
+    };
+  },
+
+  registerUser: async ({ email, password, name }) => {
+    try {
+      set({ loading: true});
+      await api.post("/users", {
+        email: email,
+        password: password,
+        name: name,
+      });
+      return true;
+    } catch (error) {
+      console.log(error);
+      set({ error: "Failed to register new account"});
+    } finally {
+      set({loading: false});
+    };
+  }
+}));
