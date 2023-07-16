0409870f080f77ac6863d9848fdb256cc4651b0b
svnoliveira <srkuran@gmail.com>
2023-07-16 12:00:09 -0300

    style: register page stylized

--- src/app/(administration)/register/page.tsx
+++ src/app/(administration)/register/page.tsx
@@ -3,7 +3,7 @@ import { RegisterForm } from "@/components/RegisterForm";
 
 export default function Register() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col lg:flex-row items-center justify-center lg:justify-start gap-10 max-w-[1448px] m-auto px-3 pt-14">
       <RegisterBanner />
       <RegisterForm />
     </main>
--- src/components/RegisterForm/index.tsx
+++ src/components/RegisterForm/index.tsx
@@ -24,27 +24,43 @@ export const RegisterForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit((formData) => parseRegisterData(formData))}>
+
+    <form onSubmit={handleSubmit((formData) => parseRegisterData(formData))}
+      className="flex flex-col justify-between w-full max-w-[568px]">
       <Link href={"/"}>
-        <button type="button">
+        <button type="button" className="flex gap-2 items-center">
           <IoMdArrowBack />
-          <span>BACK</span>
+          <span className="text-paragraph tracking-widest hover:underline decoration-solid decoration-black decoration-2">BACK</span>
         </button>
       </Link>
-      <h1>REGISTER</h1>
-      <span>Welcome, Administrator!</span>
+      <h1 className="text-title2-m md:text-title2">REGISTER</h1>
+      <span className="text-paragraph pb-2 pt-6">Welcome, Administrator!</span>
       {loading ? <span>LOADING</span>
-      :<div>
-        <input type="text" placeholder="NAME" {...register("name")} />
-        {errors.name && <p>{errors.name.message}</p>}
-        <input type="email" placeholder="E-MAIL" {...register("email")} />
-        {errors.email && <p>{errors.email.message}</p>}
-        <input type="text" placeholder="PASSWORD" {...register("password")} />
-        {errors.password && <p>{errors.password.message}</p>}
-        <input type="password" placeholder="CONFIRM PASSWORD" {...register("confirmPassword")} />
-        {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
-        <button type="submit">REGISTER</button>
-      </div>}
+        : <div className="flex flex-col gap-3">
+          <div className="relative">
+            <input className="bg-grey h-20 p-6 text-paragraph w-full"
+              type="text" placeholder="NAME" {...register("name")} />
+            {errors.name && <p className="absolute bottom-0 left-5">{errors.name.message}</p>}
+          </div>
+          <div className="relative">
+            <input className="bg-grey h-20 p-6 text-paragraph w-full"
+              type="email" placeholder="E-MAIL" {...register("email")} />
+            {errors.email && <p className="absolute bottom-0 left-5">{errors.email.message}</p>}
+          </div>
+          <div className="relative">
+            <input className="bg-grey h-20 p-6 text-paragraph w-full"
+              type="text" placeholder="PASSWORD" {...register("password")} />
+            {errors.password && <p className="pl-6">{errors.password.message}</p>}
+          </div>
+          <div className="relative">
+            <input className="bg-grey h-20 p-6 text-paragraph w-full"
+              type="password" placeholder="CONFIRM PASSWORD" {...register("confirmPassword")} />
+            {errors.confirmPassword && <p className="absolute bottom-0 left-5">{errors.confirmPassword.message}</p>}
+          </div>
+          <button type="submit"
+            className="flex bg-black text-white justify-center items-center w-full lg:w-fit px-4 py-3 hover:opacity-50 text-small-price tracking-[0.2rem] border-2 border-solid border-black"
+          >REGISTER</button>
+        </div>}
     </form>
   )
 };
